d734a1c7fefa45db395d8265f81c66a2de0a0876
Hussein Al Rubaye <hus200693@gmail.com>
2024-03-03 14:06:37 -0600

    I stopped here because i need to learn about filters and i might skip this for now

--- 02_repeatString/solution/repeatString-solution.js
+++ 02_repeatString/solution/repeatString-solution.js
@@ -1,10 +1,10 @@
-const repeatString = function (word, times) {
-  if (times < 0) return "ERROR";
-  let string = "";
-  for (let i = 0; i < times; i++) {
-    string += word;
-  }
-  return string;
+const repeatString = function () {
+
+  let string = "hey";
+  for (let i = 0; i < 9; i++) {
+    string += '';
+  };
+
 };
 
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array , ...valuesToRemove) {
+    return array.filter(item => !valuesToRemove.includes(item));
+    // i need to learn this but for now i just copied this and know little about the .filter
 };
 
 // Do not edit below this line
